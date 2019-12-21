835f06a6fea11530180d0e501ddae02203baa8bd
thelamer <ryankuba@gmail.com>
2019-12-20 16:48:00 -0800

    pulling kernel from non free CD assets

---
+++ settings.sh
@@ -0,0 +1,8 @@
+URL="https://cdimage.debian.org/cdimage/unofficial/non-free/cd-including-firmware/current-live/amd64/iso-hybrid/debian-live-REPLACE_VERSION-amd64-standard+nonfree.iso"
+TYPE=file
+CONTENTS="\
+live/initrd*|initrd
+live/vmlinuz*|vmlinuz"
+EXTRACT_INITRD="true"
+INITRD_NAME="initrd"
+INITRD_TYPE="gz"
