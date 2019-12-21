3ed8bff43ffc8dcb3ac5aeb7fb0446fc8c962339
thelamer <ryankuba@gmail.com>
2019-12-20 20:28:25 -0800

    update kali to use their kernel from the light LiveCD

---
+++ settings.sh
@@ -0,0 +1,8 @@
+URL="https://images.offensive-security.com/kali-linux-REPLACE_VERSION-amd64.iso.torrent"
+TYPE=torrent
+CONTENTS="\
+live/initrd.img|initrd
+live/vmlinuz|vmlinuz"
+EXTRACT_INITRD="true"
+INITRD_NAME="initrd"
+INITRD_TYPE="gz"
